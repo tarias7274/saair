854bbc6764635507cc72d16f5870081adce04097
tarias7274 <tristan.arias@utsa.edu>
2026-03-18 21:09:11 -0500

    Pulled set_api_key() function into its own R script following its graduation from helper to export function

--- R/get_api_key.R
+++ R/get_api_key.R
@@ -28,22 +28,3 @@ is_testing <- function() {
 testing_key <- function() {
   httr2::secret_decrypt("0Nfl6u2KHYvlt7TEFmugKxUhwNVdGNA_Lhi2KPXM17hpJK383KkI-4S8zCMzIDDUusv80Q", "SAAIR_KEY")
 }
-
-#' Set api key
-#'
-#' Prompts user to set a PurpleAir API key. Typically follows this example format:
-#' B144C732-11DE-11EE-A445-42098A784009
-#'
-#' @param key Kept NULL in order to use the obfuscating askpass interface
-#'
-#' @returns An action setting the env var `SAAIR_KEY` to input key value
-#' @export
-#'
-#' @examples
-#' set_api_key()
-set_api_key <- function(key = NULL) {
-  if (is.null(key)) {
-    key <- askpass::askpass("Please enter your API key")
-  }
-  Sys.setenv("SAAIR_KEY" = key)
-}
