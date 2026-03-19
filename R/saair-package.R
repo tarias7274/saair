6eb32d3b4b38b41e404205a89acdcf2cbd2af281
tarias7274 <tristan.arias@utsa.edu>
2026-03-18 20:34:08 -0500

    Added package-level imports for code brevity

---
+++ R/saair-package.R
@@ -0,0 +1,26 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom cli cli_alert_danger
+#' @importFrom cli cli_alert_info
+#' @importFrom cli cli_alert_warning
+#' @importFrom cli cli_progress_bar
+#' @importFrom cli cli_progress_update
+#' @importFrom dplyr filter
+#' @importFrom dplyr full_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr pull
+#' @importFrom dplyr select
+#' @importFrom lubridate as_datetime
+#' @importFrom lubridate days
+#' @importFrom lubridate force_tz
+#' @importFrom lubridate int_overlaps
+#' @importFrom lubridate interval
+#' @importFrom lubridate is.interval
+#' @importFrom readr read_csv
+#' @importFrom rlang .data
+#' @importFrom stats setNames
+#' @importFrom stringr str_sub
+## usethis namespace: end
+NULL
