4a9a07c130e2ad07c0a94e76c4e0ed27a9ea7702
tarias7274 <tristan.arias@utsa.edu>
2026-04-07 18:54:14 -0500

    Cleared Push error from dangling functions

--- tests/testthat/test-get_sensor_data.R
+++ tests/testthat/test-get_sensor_data.R
@@ -1,23 +1,23 @@
-load_test_data <- function() {
-  # Load decryption key environmental variable, if it exists
-  key <- Sys.getenv("SAAIR_KEY")
-  if (!identical(key, "")) {
-    return(key)
-  }
-  if (is_testing()) {
-    # Read in encrypted data fixture
-    testing_data <- here::here(
-      "tests", "testthat", "fixtures", "ListOfLocations_Clean.txt"
-      ) |>
-      secret_read_rds("SAAIR_KEY")
-    print(testing_data)
-      # as.data.frame() |>
-      # filter(sensor_index == 150454)
-    return(testing_data)
-  } else {
-    stop("No test data found, please supply")
-  }
-}
+# load_test_data <- function() {
+#   # Load decryption key environmental variable, if it exists
+#   key <- Sys.getenv("SAAIR_KEY")
+#   if (!identical(key, "")) {
+#     return(key)
+#   }
+#   if (is_testing()) {
+#     # Read in encrypted data fixture
+#     testing_data <- here::here(
+#       "tests", "testthat", "fixtures", "ListOfLocations_Clean.txt"
+#       ) |>
+#       secret_read_rds("SAAIR_KEY")
+#     print(testing_data)
+#       # as.data.frame() |>
+#       # filter(sensor_index == 150454)
+#     return(testing_data)
+#   } else {
+#     stop("No test data found, please supply")
+#   }
+# }
 
 is_testing <- function() {
   identical(Sys.getenv("TESTTHAT"), "true")
